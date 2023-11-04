2d57240a48d38cf91d47cc622c6ee35e6dc1334f
cuzonalan@gmail.com <cuzonalan@gmail.com>
2023-11-04 16:55:57 +0100

    add ci and cleanup test code with black & pylint

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import streamlit as st
-import pandas as pd
-import duckdb
+"""Script for SQL-SRS application streamlit."""
+
 import io
 
+import duckdb
+import pandas as pd
+import streamlit as st
 
 csv = """
 beverage, price
@@ -22,44 +24,59 @@ muffin, 3
 
 food_items = pd.read_csv(io.StringIO(csv2))
 
-answer = """
+ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-solution = duckdb.sql(answer).df()
+solution_df = duckdb.sql(ANSWER_STR).df()
 
 
 with st.sidebar:
-
     option = st.selectbox(
         "What would you like to review ?",
-        ['Joins', 'GroupBy', 'Windows Functions'],
+        ["Joins", "GroupBy", "Windows Functions"],
         index=None,
-        placeholder='Select a theme ...'
+        placeholder="Select a theme ...",
     )
 
-    st.write('You selected:', option)
+    st.write("You selected:", option)
 
-st.header('Entrez votre requête :')
+st.header("Entrez votre requête :")
 
-query = st.text_area(label="Votre code SQL ici", key='user_input')
+query = st.text_area(label="Votre code SQL ici", key="user_input")
 
-if query :
+if query:
+    result = duckdb.sql(query).df()
+    st.dataframe(result)
 
-   result = duckdb.sql(query).df()
-   st.dataframe(result)
+    if len(result.columns) != len(solution_df.columns):
+        st.error("Some columns are missing")
 
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
 
-tab2, tab3 = st.tabs(['Tables', 'Solutions'])
+    except KeyError as e:
+        st.error("Some columns are missing")
+
+    n_len_differences = result.shape[0] - solution_df.shape[0]
+
+    if n_len_differences != 0:
+        st.error(
+            f"Result has a {n_len_differences} lines difference with the solution_df"
+        )
+
+tab2, tab3 = st.tabs(["Tables", "solution_dfs"])
+
 
 with tab2:
-   st.write("table: beverages")
-   st.dataframe(beverages)
-   st.write("table: food_items")
-   st.dataframe(food_items)
-   st.write("expected")
-   st.dataframe(solution)
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected")
+    st.dataframe(solution_df)
 
 with tab3:
-   st.write(answer)
+    st.write(ANSWER_STR)

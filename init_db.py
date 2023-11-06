00caa38fb4031b5369a77a94ced84e89886b4038
cuzonalan@gmail.com <cuzonalan@gmail.com>
2023-11-06 15:56:00 +0100

    comparison with real answer

--- init_db.py
+++ init_db.py
@@ -6,7 +6,7 @@ import duckdb
 import pandas as pd
 import streamlit as st
 
-con = duckdb.connect('data/exercices_sql_tables.duckdb', read_only=False)
+con = duckdb.connect("data/exercices_sql_tables.duckdb", read_only=False)
 
 
 # --------------------------------------------------
@@ -14,14 +14,14 @@ con = duckdb.connect('data/exercices_sql_tables.duckdb', read_only=False)
 # --------------------------------------------------
 
 data = {
-    'theme': ['cross_joins', 'window_functions'],
-    'exercise_name': ['beverages_and_food', 'simple_window'],
-    'tables': [['beverages', 'food_items'], 'simple_windows'],
-    'last_reviewed': ['1970-01-01', '1970-01-01'],
-    }
+    "theme": ["cross_joins", "window_functions"],
+    "exercise_name": ["beverages_and_food", "simple_window"],
+    "tables": [["beverages", "food_items"], "simple_windows"],
+    "last_reviewed": ["1970-01-01", "1970-01-01"],
+}
 
 memory_state_df = pd.DataFrame(data)
-con.execute('CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df')
+con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
 
 # --------------------------------------------------
 # CROSS JOINS EXERCICES
